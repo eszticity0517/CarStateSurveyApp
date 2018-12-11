98200c46bca58f0238982da659ee184c2beaf62b
Eszter Szabó <eszticity@hotmail.com>
2018-12-11 22:21:25 +0100

    Body Aesthetic page is sketched

--- app/src/main/java/com/carstatesurveyapp/GeneralInfo.java
+++ app/src/main/java/com/carstatesurveyapp/GeneralInfo.java
@@ -1,9 +1,11 @@
 package com.carstatesurveyapp;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
@@ -46,6 +48,14 @@ public class GeneralInfo extends AppCompatActivity {
                 new DatePickerDialog(GeneralInfo.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        Button nextbutton = findViewById(R.id.button4);
+        nextbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent k = new Intent(GeneralInfo.this, BodyAesthetic.class);
+                startActivity(k);
             }
         });
     }
