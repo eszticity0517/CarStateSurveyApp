005d154237c706438eb9ff4cbb2dde65af715507
Eszter Szabó <eszticity@hotmail.com>
2018-12-11 17:26:17 +0100

    Generic info sketch is done + BodyAesthetic is added

---
+++ app/src/main/java/com/carstatesurveyapp/BodyAesthetic.java
@@ -0,0 +1,13 @@
+package com.carstatesurveyapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BodyAesthetic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_body_aesthetic);
+    }
+}
--- app/src/main/java/com/carstatesurveyapp/GeneralInfo.java
+++ app/src/main/java/com/carstatesurveyapp/GeneralInfo.java
@@ -1,13 +1,59 @@
 package com.carstatesurveyapp;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class GeneralInfo extends AppCompatActivity {
+    private EditText receptiondate;
+    private Calendar myCalendar;
+    private  DatePickerDialog.OnDateSetListener date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_general_info2);
+
+        myCalendar = Calendar.getInstance();
+        receptiondate = (EditText) findViewById(R.id.receptiondatetext);
+
+        date = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                // TODO Auto-generated method stub
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+        };
+
+        receptiondate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(GeneralInfo.this, date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        receptiondate.setText(sdf.format(myCalendar.getTime()));
     }
 }
