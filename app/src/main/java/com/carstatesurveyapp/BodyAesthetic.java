c06bf6dd361e702606e1760ebb22693166d9300c
Eszter Szabó <eszticity@hotmail.com>
2018-12-12 14:00:58 +0100

    PaintThickness slider numbers are added

--- app/src/main/java/com/carstatesurveyapp/BodyAesthetic.java
+++ app/src/main/java/com/carstatesurveyapp/BodyAesthetic.java
@@ -1,8 +1,10 @@
 package com.carstatesurveyapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 
 public class BodyAesthetic extends AppCompatActivity {
@@ -11,6 +13,14 @@ public class BodyAesthetic extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body_aesthetic);
+
+        Button nextbutton = findViewById(R.id.button4);
+        nextbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent k = new Intent(BodyAesthetic.this, PaintThickness.class);
+                startActivity(k);
+            }
+        });
     }
 
     public void onRadioButtonClicked(View view) {
